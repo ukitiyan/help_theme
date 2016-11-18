e1e5dde4dfc171f86416e3b69f1bc397e5aefb1c
k-ooshiro <kanichi.oshiro@stylez.co.jp>
2016-11-18 13:02:13 +0900

    fix oc9

--- settings/templates/help.php
+++ settings/templates/help.php
@@ -1,17 +1,17 @@
 <div id="app-navigation">
 	<ul>
 		<li>
-			<a href="<?php print_unescaped(OC_Helper::linkToAbsolute('themes', "default/settings/pdf/manual1.pdf")); ?>" >
+			<a href="<?php print_unescaped(\OCP\Util::linkToAbsolute('themes', "default/settings/pdf/manual1.pdf")); ?>" >
 				マニュアル1
 			</a>
 		</li>
 		<li>
-			<a href="<?php print_unescaped(OC_Helper::linkToAbsolute('themes', "default/settings/pdf/manual2.pdf")); ?>">
+			<a href="<?php print_unescaped(\OCP\Util::linkToAbsolute('themes', "default/settings/pdf/manual2.pdf")); ?>">
 				マニュアル2
 			</a>
 		</li>
 		<li>
-			<a href="<?php print_unescaped(OC_Helper::linkToAbsolute('themes', "default/settings/pdf/manual3.pdf")); ?>">
+			<a href="<?php print_unescaped(\OCP\Util::linkToAbsolute('themes', "default/settings/pdf/manual3.pdf")); ?>">
 				マニュアル3
 			</a>
 		</li>
@@ -21,4 +21,4 @@
 	<iframe id="frame" src="<?php print_unescaped($_['url']); ?>" class="help-iframe">
 	</iframe>
 </div>
-<script type="text/javascript" src="<?php print_unescaped(OC_Helper::linkToAbsolute('themes', 'default/settings/js/help.js')) ?>"></script>
+<script type="text/javascript" src="<?php print_unescaped(\OCP\Util::linkToAbsolute('themes', 'default/settings/js/help.js')) ?>"></script>
